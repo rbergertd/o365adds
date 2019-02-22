5e0fa0907c5a90561eb552bb49b9dd5247f65377
rbergertd <45945983+rbergertd@users.noreply.github.com>
2019-02-22 12:18:40 -0500

    Create getADC.ps1

---
+++ getADC.ps1
@@ -0,0 +1,6 @@
+#Variables
+$URI = "https://download.microsoft.com/download/B/0/0/B00291D0-5A83-4DE7-86F5-980BC00DE05A/AzureADConnect.msi"
+#Create Directory
+New-Item -ItemType "directory" -Path "C:\AzureADConnect\" 
+#Get Azure AD Connect installer
+Invoke-WebRequest -UseBasicparsing -Uri $URI -OutFile "C:\AzureADConnect\AzureADConnect.msi"
