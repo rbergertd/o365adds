2c23cf9333853c0504c68acd7b7030a7b7b9930f
rbergertd <45945983+rbergertd@users.noreply.github.com>
2019-03-11 13:50:55 -0400

    Adding a dependancy on getADC for joinDomain - restart was causing conflicts.

--- getADC.ps1
+++ getADC.ps1
@@ -1,5 +1,3 @@
-    #Wait for ADC reboot after domainJoin
-    Start-Sleep -s 240
     #Variables
     $URI = "https://download.microsoft.com/download/B/0/0/B00291D0-5A83-4DE7-86F5-980BC00DE05A/AzureADConnect.msi"
     #Create Directory
