7e294528b208cf1960e4afd3a8837bf580ba9fb9
rbergertd <45945983+rbergertd@users.noreply.github.com>
2019-03-11 12:25:36 -0400

    Update getADC.ps1

--- getADC.ps1
+++ getADC.ps1
@@ -1,8 +1,8 @@
-#Variables
-$URI = "https://download.microsoft.com/download/B/0/0/B00291D0-5A83-4DE7-86F5-980BC00DE05A/AzureADConnect.msi"
-#Wait for ADC reboot after domainJoin
-Start-Sleep -s 240
-#Create Directory
-New-Item -ItemType "directory" -Path "C:\AzureADConnect\" 
-#Get Azure AD Connect installer
-Invoke-WebRequest -UseBasicparsing -Uri $URI -OutFile "C:\AzureADConnect\AzureADConnect.msi"
+    #Wait for ADC reboot after domainJoin
+    Start-Sleep -s 240
+    #Variables
+    $URI = "https://download.microsoft.com/download/B/0/0/B00291D0-5A83-4DE7-86F5-980BC00DE05A/AzureADConnect.msi"
+    #Create Directory
+    New-Item -ItemType "directory" -Path "C:\AzureADConnect\" 
+    #Get Azure AD Connect installer
+    Invoke-WebRequest -UseBasicparsing -Uri $URI -OutFile "C:\AzureADConnect\AzureADConnect.msi"
