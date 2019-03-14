82e1d0358bcef7f6a12f07d35e2c39432776e025
Ryan Berger <ryan.berger@techdata.com>
2019-03-14 16:21:23 -0400

    Changing DSC

---
+++ dsc_inprog/builddc/dsc_createdomain.ps1
@@ -0,0 +1,133 @@
+﻿configuration CreateADPDC 
+{ 
+   param 
+   ( 
+        [Parameter(Mandatory)]
+        [String]$DomainName,
+
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$Admincreds,
+
+        [Int]$RetryCount=20,
+        [Int]$RetryIntervalSec=30
+    ) 
+    
+    Import-DscResource -ModuleName xActiveDirectory, xStorage, xNetworking, PSDesiredStateConfiguration, xPendingReboot
+    [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
+    $Interface=Get-NetAdapter|Where Name -Like "Ethernet*"|Select-Object -First 1
+    $InterfaceAlias=$($Interface.Name)
+
+    Node localhost
+    {
+        LocalConfigurationManager 
+        {
+            RebootNodeIfNeeded = $true
+        }
+
+	    WindowsFeature DNS 
+        { 
+            Ensure = "Present" 
+            Name = "DNS"		
+        }
+
+        Script EnableDNSDiags
+	    {
+      	    SetScript = { 
+		        Set-DnsServerDiagnostics -All $true
+                Write-Verbose -Verbose "Enabling DNS client diagnostics" 
+            }
+            GetScript =  { @{} }
+            TestScript = { $false }
+	        DependsOn = "[WindowsFeature]DNS"
+        }
+
+	    WindowsFeature DnsTools
+	    {
+	        Ensure = "Present"
+            Name = "RSAT-DNS-Server"
+            DependsOn = "[WindowsFeature]DNS"
+	    }
+
+        xDnsServerAddress DnsServerAddress 
+        { 
+            Address        = '127.0.0.1' 
+            InterfaceAlias = $InterfaceAlias
+            AddressFamily  = 'IPv4'
+	        DependsOn = "[WindowsFeature]DNS"
+        }
+
+        xWaitforDisk Disk2
+        {
+            DiskNumber = 2
+            RetryIntervalSec =$RetryIntervalSec
+            RetryCount = $RetryCount
+        }
+
+        xDisk ADDataDisk 2 {
+            DiskNumber = 2
+            DriveLetter = "F"
+            FSLabel = "AD"
+            DependsOn = "[xWaitForDisk]Disk2"
+        }
+
+        xWaitforDisk Disk3
+        {
+            DiskNumber = 3
+            RetryIntervalSec =$RetryIntervalSec
+            RetryCount = $RetryCount
+        }
+
+        xDisk ADDataDisk 3 {
+            DiskNumber = 3
+            DriveLetter = "G"
+            FSLabel = "Data"
+            DependsOn = "[xWaitForDisk]Disk3"
+        }
+<#
+        cDiskNoRestart ADDataDisk
+        {
+            DiskNumber = 2
+            DriveLetter = "F"
+        }
+#>
+        WindowsFeature ADDSInstall 
+        { 
+            Ensure = "Present" 
+            Name = "AD-Domain-Services"
+	        DependsOn="[WindowsFeature]DNS" 
+        } 
+
+        WindowsFeature ADDSTools
+        {
+            Ensure = "Present"
+            Name = "RSAT-ADDS-Tools"
+            DependsOn = "[WindowsFeature]ADDSInstall"
+        }
+
+        WindowsFeature ADAdminCenter
+        {
+            Ensure = "Present"
+            Name = "RSAT-AD-AdminCenter"
+            DependsOn = "[WindowsFeature]ADDSInstall"
+        }
+         
+        xADDomain FirstDS 
+        {
+            DomainName = $DomainName
+            DomainAdministratorCredential = $DomainCreds
+            SafemodeAdministratorPassword = $DomainCreds
+            DatabasePath = "F:\NTDS"
+            LogPath = "F:\NTDS"
+            SysvolPath = "F:\SYSVOL"
+	        DependsOn = "[xDisk]ADDataDisk 2"
+        } 
+
+        xADRecycleBin RecycleBin            
+        {            
+           EnterpriseAdministratorCredential = $DomainCreds            
+           ForestFQDN = $DomainName            
+           DependsOn = '[xADDomain]FirstDS'            
+        }   
+
+   }
+} 
